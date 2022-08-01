513b493db65222e08f473e4e056cc0294424118a
Crowdbotics <team@crowdbotics.com>
2022-08-01 23:21:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "helicopter_institut_36551",
+  url: "https://helicopter_institut_36551.botics.co",
+  api: "https://helicopter_institut_36551.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
